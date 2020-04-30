a00c1b42ec86f9eaf5a140a0ac0f06b7e670ab50
Samuel Guimarães <samuca.guimaraes@gmail.com>
2020-04-29 23:56:01 -0300

    - Configuração no tsconfig.json para incluir pasta de config/assets.ts - Tela de lista de casos inciada

--- mobile/src/pages/Incidents/index.tsx
+++ mobile/src/pages/Incidents/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
 
-import { assets } from 'config/assets';
+import { assets } from '../../config/assets';
 import styles from './styles';
 
 export default function Incidents(){
@@ -15,7 +16,67 @@ export default function Incidents(){
             </View>
 
             <Text style={styles.title}>Bem-Vindo!</Text>
-            <Text style={styles.decription}></Text>
+            <Text style={styles.decription}>Escolha um dos casos abaixo e salve o dia.</Text>
+
+        <View style={styles.incidentList}>
+            <View style={styles.incident} >
+                <Text style={styles.incidentProperty}> ONG: </Text>
+                <Text style={styles.incidentValue}> APAD: </Text>
+               
+                <Text style={styles.incidentProperty}> CASO: </Text>
+                <Text style={styles.incidentValue}> Cadelinha Atropelada </Text>
+               
+                <Text style={styles.incidentProperty}> Valor: </Text>
+                <Text style={styles.incidentValue}> R$ 120,00</Text>
+                
+                <TouchableOpacity 
+                    style={styles.detailButton}
+                >
+                    <Text style={styles.detailsButtonText}> ver mais detalhes</Text>
+                    <Feather name="arrow-right" size={16} color="#E02041"></Feather>
+                </TouchableOpacity>
+             
+                 </View>
+
+                 <View style={styles.incident} >
+                <Text style={styles.incidentProperty}> ONG: </Text>
+                <Text style={styles.incidentValue}> APAD: </Text>
+               
+                <Text style={styles.incidentProperty}> CASO: </Text>
+                <Text style={styles.incidentValue}> Cadelinha Atropelada </Text>
+               
+                <Text style={styles.incidentProperty}> Valor: </Text>
+                <Text style={styles.incidentValue}> R$ 120,00</Text>
+                
+                <TouchableOpacity 
+                    style={styles.detailButton}
+                >
+                    <Text style={styles.detailsButtonText}> ver mais detalhes</Text>
+                    <Feather name="arrow-right" size={16} color="#E02041"></Feather>
+                </TouchableOpacity>
+             
+                 </View>
+
+                 <View style={styles.incident} >
+                <Text style={styles.incidentProperty}> ONG: </Text>
+                <Text style={styles.incidentValue}> APAD: </Text>
+               
+                <Text style={styles.incidentProperty}> CASO: </Text>
+                <Text style={styles.incidentValue}> Cadelinha Atropelada </Text>
+               
+                <Text style={styles.incidentProperty}> Valor: </Text>
+                <Text style={styles.incidentValue}> R$ 120,00</Text>
+                
+                <TouchableOpacity 
+                    style={styles.detailButton}
+                >
+                    <Text style={styles.detailsButtonText}> ver mais detalhes</Text>
+                    <Feather name="arrow-right" size={16} color="#E02041"></Feather>
+                </TouchableOpacity>
+             
+                 </View>
+
+            </View>
         </View>
     );
 }
